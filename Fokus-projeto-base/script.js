1440b5a0be84baaa2565a6c74c1a8874f4296cf5
Lorena200216 <midia.faustino@scania.com>
2023-12-13 15:50:54 -0300

    novo projeto fokus

--- Fokus-projeto-base/script.js
+++ Fokus-projeto-base/script.js
@@ -13,7 +13,7 @@ const audioPlay = new Audio('sons/play.wav')
 const audioPause = new Audio('sons/pause.mp3')
 const audioTempoFinalizado = new Audio('sons/beep.mp3')
 const iniciarOuPausarBt = document.querySelector('#start-pause span')
-const tempoNaTela = document.querySelector('#timer')/**/
+const tempoNaTela = document.querySelector('#timer')
 
 let tempoDecorridoEmSegundos = 1500  /*25min*/
 let intervaloID = null
@@ -30,17 +30,20 @@ musicaFocoInput.addEventListener('change', () => { /*change input que usa mais p
 
 /*Essencialmente, este código está manipulando a aparência ou o comportamento da página quando os botões específicos são clicados, alterando dinamicamente o valor de um atributo no elemento HTML principal.*/
 
-    focoBT.addEventListener('click', () => {/*focoBT. Quando esse botão é clicado, a função dentro do bloco de código é executada.  */   
+    focoBT.addEventListener('click', () => {/*focoBT. Quando esse botão é clicado, a função dentro do bloco de código é executada.escutar a interação com a pagina */   
+        tempoDecorridoEmSegundos = 1500/*tempo de cada descanso 25m*/        
         alterarContexto('foco')
         focoBT.classList.add('active')/*adicionando a class "active"*/
 })
 
     curtoBT.addEventListener('click', () => { /*Crie uma função chamada "Alterar Contexto" para automatizar a mudança de imagens, cores de fundo dos elementos e textos;*/
+        tempoDecorridoEmSegundos = 300 /*tempo de cada descanso 5m*/
         alterarContexto('descanso-curto')
-        curtoBT.classList.add('active')
+        curtoBT.classList.add('active')/*class para mudar a classes e estilo dos botões */
 })
 
     longoBT.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900 /*tempo de cada descanso 15m*/
     alterarContexto('descanso-longo')
     longoBT.classList.add('active')
 
@@ -52,15 +55,16 @@ musicaFocoInput.addEventListener('change', () => { /*change input que usa mais p
     
     /*ClassList fornece métodos que facilitam a adição, remoção e verificação de classes, tornando a manipulação de classes CSS mais eficiente e menos suscetível a erros de programação.*/
     function alterarContexto(contexto){
+        mostrarTempo()/*aparecendo cada tempo no área de descanso correspondente*/
         botoes.forEach(function (contexto){
             contexto.classList.remove('active')/* remover a marcação de todos os botões quando acionado ( foco, Descanso ...)*/
 
         })
 
     html.setAttribute('data-contexto', contexto) /*setAttribute() Adiciona um novo atributo ou modifica o valor de um atributo existente num elemento específico.*/
-    banner.setAttribute('src', `imagens/${contexto}.png`)/* mudando a imagem */
-    switch (contexto) { /* mudando o texto quando clicar , "innerHTML" para alterar o texto exibido na tela de acordo com o contexto selecionado */
-        case 'foco':
+    banner.setAttribute('src', `imagens/${contexto}.png`)/* mudando o caminho das imagens */
+    switch (contexto) { 
+        case 'foco': /* mudando o texto quando clicar , "innerHTML" para alterar o texto exibido na tela de acordo com o contexto selecionado */
             titulo.innerHTML = `
             Otimize sua produtividade,<br>
             <strong class="app__title-strong">mergulhe no que importa.</strong>
@@ -89,6 +93,7 @@ const contagemRegressiva = () => { /*criar funcão dentro de uma constante para 
         zerar() /*ela torna o intervalo ID null, depois que o alert aparecer na tela*/
         return
     }
+
     tempoDecorridoEmSegundos -= 1
     mostrarTempo()
    
@@ -113,10 +118,27 @@ function zerar(){ /*vai interromper a excecução de algum codigo, pra não faze
     intervaloID = null
 }
 
-function mostrarTempo(){ /*colocando o tempo na tela*/
+function mostrarTempo() {
     const tempo = new Date(tempoDecorridoEmSegundos * 1000)
-    const tempoFormatado = tempo.toLocaleDateString('pt-br', {minute:'2-digit', second:'2-digit'}) /*date.toLocaleDateString()é usado para converter uma data em uma string.*/
-    tempoNaTela.innerHTML = `${tempoFormatado}`
+    const tempoFormatado = tempo.toLocaleTimeString('pt-Br', {minute: '2-digit', second:'2-digit'}) /*toLocaleTimeString é usada para obter de string da hora de um objeto Data, formatada de acordo com as configurações de localidade (locale) e opções fornecidas.*/
+   tempoNaTela.innerHTML = `${tempoFormatado}`
 }
 
-mostrarTempo() /*deixar fixo o tempo na tela*/
+mostrarTempo() /*deixar fixo o tempo na tela*/// Em qualquer lugar que você queira chamar a função mostrarTempo(), como no final do script
+
+
+/* como colocar relogio hora/min/segundos  *****EXEMPLO***
+const dataAtual = new Date();
+
+const opcoes = {
+  hour: '2-digit',
+  minute: '2-digit',
+  second: '2-digit',
+  timeZoneName: 'short'
+};
+
+const formatoLocalizado = dataAtual.toLocaleTimeString('pt-BR', opcoes);
+
+console.log(formatoLocalizado);
+
+*/
